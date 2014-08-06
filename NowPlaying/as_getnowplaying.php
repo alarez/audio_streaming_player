66827d7e874ea5c5907f0963d78bde0d3583106d
Alain  Martínez <alain@alarezmedia.com>
2014-08-06 15:37:57 -0600

    Initial Commit.

---
+++ NowPlaying/as_getnowplaying.php
@@ -0,0 +1,52 @@
+<?php
+//The stream url for obtain icecast metadata
+$stream_url = $_POST['stream_url'];
+//Result, false if the file does not contain metadata, otherwise will contain the stream information
+$result = false;
+$icy_metaint = -1;
+$needle = 'StreamTitle=';
+$ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36';
+
+
+$opts = array(
+  'http' => array(
+    'method' => 'GET',
+    'header' => 'Icy-MetaData: 1',
+    'user_agent' => $ua
+  )
+);
+
+$default = stream_context_set_default($opts);
+//Open the stream
+$stream = fopen($stream_url, 'r');
+
+//Ask it the file contains metadata
+if ($stream && ($meta_data = stream_get_meta_data($stream)) && isset($meta_data['wrapper_data'])) {
+  foreach ($meta_data['wrapper_data'] as $header) {
+    if (strpos(strtolower($header), 'icy-metaint') !== false) {
+      $tmp = explode(":", $header);
+      $icy_metaint = trim($tmp[1]);
+      break;
+    }
+  }
+}
+
+if ($icy_metaint != -1) {
+  $buffer = stream_get_contents($stream, 300, $icy_metaint);
+  //get information from the stream url
+  if (strpos($buffer, $needle) !== false) {
+    $title = explode($needle, $buffer);
+    $title = trim($title[1]);
+    $result = substr($title, 1, strpos($title, ';') - 2);
+    $result = substr($title, 1, strpos($title, ';') - 2);
+    
+    if(strlen($result)>39){
+      $result=  substr($result,0,39);
+    }
+  }
+}
+//Close the stream
+if ($stream)
+  fclose($stream);
+//Show the result
+echo $result;
---
+++ themes/audio_streaming_player_circular/js/audio_streaming_player_circular.js
@@ -0,0 +1,82 @@
+(function($) {
+    'use strict';
+    Drupal.behaviors.audiostreamingplayer = {
+        attach: function(context, settings) {
+            var pause_ = true;
+            var startmessage = Drupal.t('Click Play to start listening');
+            $("#responsecontainer").html(startmessage);
+            var stream = {
+                //Atributtes: title and stream url
+                title: startmessage,
+                mp3: Drupal.settings.audiostreamingplayer.stream_url
+            },
+            ready = false;
+
+            $("#audio_streaming_player").jPlayer({
+                ready: function(event) {
+                    ready = true;
+                    
+                    //If the variable auto-play is on, then starts playing from the beginning
+                    if (Drupal.settings.audiostreamingplayer.auto_play === 1) {
+                        $(this).jPlayer("setMedia", stream).jPlayer("play");
+                        pause_ = false;
+                    }
+
+                },
+                pause: function() {
+                    //Pause audio playback
+                    $(this).jPlayer("clearMedia");
+                    pause_ = true;
+                },
+                
+                error: function(event) {
+
+                    if (ready && event.jPlayer.error.type === $.jPlayer.error.URL_NOT_SET) {
+                        
+                        pause_ = false;
+                        // Setup the media stream again and play it.
+                        $(this).jPlayer("setMedia", stream).jPlayer("play");
+
+                    }
+                },
+                solution: 'flash, html',
+                //swf path
+                swfPath: Drupal.settings.audiostreamingplayer.swf,
+                //Allowed Formats
+                supplied: "mp3,oga",
+                preload: "none",
+                wmode: "window",
+                keyEnabled: true
+            });
+            //Get the metadata from the stream
+
+            var refreshId = setInterval(function() {
+
+                if (!pause_) {
+                    var parameters = {
+                        "stream_url": Drupal.settings.audiostreamingplayer.stream_url
+                    };
+                    //ajax call to get the metadata
+                    $.ajax({
+                        data: parameters,
+                        url: '/sites/all/modules/audio_streaming_player/NowPlaying/as_getnowplaying.php',
+                        type: 'post',
+                        beforeSend: function() {
+
+                        },
+                        success: function(response) {
+                            //Load the result in the container
+                            $("#responsecontainer").html(response);
+                        }
+                    });
+
+                }
+                else {
+                    $("#responsecontainer").html(startmessage);
+                }
+            }, 6000);
+        }
+
+    };
+}(jQuery));
+
